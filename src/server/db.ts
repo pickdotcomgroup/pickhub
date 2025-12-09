e5f630b4508236dbee463bf6fb80c22ef6b44f7a
Marven Paray <68675200+Nevram30@users.noreply.github.com>
2025-12-09 12:46:04 +0800

    feat: fix connection

--- src/server/db.ts
+++ src/server/db.ts
@@ -4,6 +4,13 @@ const createPrismaClient = () =>
   new PrismaClient({
     log:
       process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL?.includes('?')
+          ? `${process.env.DATABASE_URL}&pgbouncer=true&connect_timeout=15&prepared_statements=false`
+          : `${process.env.DATABASE_URL}?pgbouncer=true&connect_timeout=15&prepared_statements=false`,
+      },
+    },
   });
 
 const globalForPrisma = globalThis as unknown as {
